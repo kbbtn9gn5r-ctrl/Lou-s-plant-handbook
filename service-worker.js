5bcc78ac641031e362071326348dcf10d1ce00c4
kbbtn9gn5r-ctrl <mandmred@msn.com>
2026-07-26 19:11:56 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,31 @@
-const CACHE='lous-garden-v1';
-const ASSETS=['./','./index.html','./indoor-plants.html','./manifest.webmanifest',
-'./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE = 'lous-garden-guide-v2-2026-07-26';
+const CORE = [
+  './', './index.html', './journal.html', './photo-journal.html',
+  './garden-updates-2026.html', './indoor-plants.html',
+  './manifest.webmanifest', './icons/icon-192.png',
+  './icons/icon-512.png', './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
