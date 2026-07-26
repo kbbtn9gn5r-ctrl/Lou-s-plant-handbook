5efdd7c756efa80e77290ae3b2e9a1cfed25e8c6
kbbtn9gn5r-ctrl <mandmred@msn.com>
2026-07-26 18:45:04 -0400

    Add files via upload

--- service-worker-v2.js
+++ service-worker-v2.js
@@ -1,4 +1,4 @@
-const CACHE='lous-garden-welcome-v2';
+const CACHE='lous-garden-welcome-v3';
 const ASSETS=['./','./index.html?v=2','./manifest-v2.webmanifest',
 './icons/icon-192-v2.png?v=2','./icons/icon-512-v2.png?v=2','./icons/apple-touch-icon-v2.png?v=2'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
